c72fe552e7ec00e5bdccc12f439dac1af3b97139
Ly659 <2011202398@qq.com>
2026-05-30 15:29:10 +0800

    [2026.5.30] BUG修复：大修MainActivity权限管理体系

--- app/src/main/java/com/ly659/easybackup2/MainActivity.java
+++ app/src/main/java/com/ly659/easybackup2/MainActivity.java
@@ -32,7 +32,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * 用于集中管理申请的权限，包含所有可以请求的封装好的权限。
@@ -53,43 +52,64 @@ class Permissions {
     }
 
     // 所有可能申请的的权限类型
-    public static final Permission PERMISSION_NETWORK =
-            new Permission(Manifest.permission.INTERNET, 100);              // 网络权限
-    public static final Permission PERMISSION_STORAGE_READ =
-            new Permission(Manifest.permission.READ_EXTERNAL_STORAGE, 101); // 文件读取权限
+    /**
+     * 权限数组索引：
+     * [0] 网络权限
+     * [1] 文件读取权限
+     */
+    static final Permission[] permissions = new Permission[] {
+        new Permission(Manifest.permission.INTERNET, 100),              // 网络权限
+        new Permission(Manifest.permission.READ_EXTERNAL_STORAGE, 101)  // 文件读取权限
+    };
 }
 
 public class MainActivity extends AppCompatActivity {
 
     /**
-     * 向用户请求指定的权限。
+     * 检查指定的权限是否已经获得。
      * @param permission 指定权限
-     * @return 权限是否成功获取。若成功获取（用户授权），返回true；否则返回false。
-     * 注：若权限已经拥有，无需再请求，返回true。
+     * @return 若已获得，则返回true；否则返回false。
      */
-    private boolean askPermission(Permissions.Permission permission) {
-        // 检查权限是否已经拥有
-        if (ActivityCompat.checkSelfPermission(this, permission.permissionName) == PackageManager.PERMISSION_GRANTED) return true;
-        // 向用户请求权限
-        ActivityCompat.requestPermissions(this, new String[] {permission.permissionName}, permission.requestCode);
-        // 再次检查权限是否获得，若成功获得则返回true，否则返回false
+    private boolean checkPermission(@NonNull Permissions.Permission permission) {
         return ActivityCompat.checkSelfPermission(this, permission.permissionName) == PackageManager.PERMISSION_GRANTED;
     }
 
-    private void startConnect(@NonNull SetConnectPackage returnPack) {
-        // 封装用户选择的文件类型选项
-        List<TypeInfo> userTypes = new ArrayList<>();
-        //if (returnPack.fileType_videos) userTypes.add(new TypeInfo(Types.Video, 999));
-        if (returnPack.fileType_photos) userTypes.add(new TypeInfo(Types.Photo, FileHelper.getNum_Photos()));
+    /**
+     * 向用户请求指定的权限。
+     * 注：如果指定的权限已经取得，执行此方法不会有任何效果。要检查权限是否获得，请参照Permission变量。
+     * @param permission 指定权限
+     */
+    private void askPermission(@NonNull Permissions.Permission permission) {
+        // 检查权限是否已经拥有
+        if (ActivityCompat.checkSelfPermission(this, permission.permissionName) == PackageManager.PERMISSION_GRANTED) return;
+        // 向用户请求权限
+        ActivityCompat.requestPermissions(this, new String[] {permission.permissionName}, permission.requestCode);
+    }
 
-        // 获取并启动网络连接线程
-        Thread thread_Network = getThread_network(returnPack, userTypes);
-        thread_Network.setName("Thread_NETWORK");
-        thread_Network.start();
+    /**
+     * 开始与PC端的网络连接和数据传输操作。
+     * @param returnPack 用户自定义的连接参数
+     */
+    private void startConnect(@NonNull SetConnectPackage returnPack) {
+        // 获取必要的权限（若获取失败则终止操作）
+        if (checkPermission(Permissions.permissions[0]) && checkPermission(Permissions.permissions[1])) {
+            // 封装用户选择的文件类型选项
+            List<TypeInfo> userTypes = new ArrayList<>();
+            if (returnPack.fileType_videos) userTypes.add(new TypeInfo(Types.Video, 999));
+            if (returnPack.fileType_photos) userTypes.add(new TypeInfo(Types.Photo, FileHelper.getNum_Photos()));
+
+            // 获取并启动网络连接线程
+            Thread thread_Network = getThread_network(returnPack, userTypes);
+            thread_Network.setName("Thread_NETWORK");
+            thread_Network.start();
+        } else {
+            Toast.makeText(this, "若要继续操作，请您授予必要的权限。", Toast.LENGTH_LONG).show();
+        }
     }
 
     @NonNull
     private Thread getThread_network(@NonNull SetConnectPackage returnPack, List<TypeInfo> userTypes) {
+        // 封装握手信息
         ConnectionHand handPack = new ConnectionHand(FileHelper.getName_Phone(), returnPack.hostIp, returnPack.hostPort, userTypes);
 
         // 建立与PC端的连接
@@ -148,5 +168,9 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, SetConnectActivity.class);
             luncher_SetConnect.launch(intent);
         }));
+
+        // 获取权限
+        askPermission(Permissions.permissions[0]);
+        askPermission(Permissions.permissions[1]);
     }
 }
