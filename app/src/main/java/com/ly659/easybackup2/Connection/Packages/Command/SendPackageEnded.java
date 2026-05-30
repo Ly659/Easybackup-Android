d9f828099aebdcb0a7910b884471c2a2b4e43540
Ly659 <2011202398@qq.com>
2026-05-30 15:25:56 +0800

    [2026.5.30] BUG修复：SendPackageEnded不一致问题

--- app/src/main/java/com/ly659/easybackup2/Connection/Packages/Command/SendPackageEnded.java
+++ app/src/main/java/com/ly659/easybackup2/Connection/Packages/Command/SendPackageEnded.java
@@ -1,10 +1,13 @@
 package com.ly659.easybackup2.Connection.Packages.Command;
 
 
+import java.io.Serial;
 import java.io.Serializable;
 
 /**
  * 此包用于表示发送端的所有数据都已经发送完毕，接收端可以执行清理操作。
  */
 public class SendPackageEnded implements Command, Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 }
